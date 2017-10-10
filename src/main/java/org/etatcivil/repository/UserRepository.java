29767a1919bce60e9a505964fd755ba5351390cd
sowoumar <mr.sowoumar@gmail.com>
2017-10-10 23:11:36 +0200

    userRepository

---
+++ src/main/java/org/etatcivil/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package org.etatcivil.repository;
+
+import org.etatcivil.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Spring Data JPA repository for the User entity.
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findOneByName(String name);
+}
